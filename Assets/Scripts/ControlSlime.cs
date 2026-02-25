fde9cb4e06cbeac0287eec856ee5fc6e2e20fa80
EkkoDev <rey.alexisamparo@gmail.com>
2026-02-25 18:23:37 -0400

    Nuevos sprites y corrección de errores visuales

--- Assets/Scripts/ControlSlime.cs
+++ Assets/Scripts/ControlSlime.cs
@@ -1,40 +1,37 @@
 using UnityEngine;
 
-public class ControlSlime : MonoBehaviour
+public class ControlJugador : MonoBehaviour
 {
-    [Header("Configuracion de Movimiento")]
-    public float velocidad = 7f;
-    public float fuerzaSalto = 15f;
+    public float velocidad = 8f;
+    public float fuerzaSalto = 12f;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator anim;
 
     void Start()
     {
-        
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
         float mover = Input.GetAxis("Horizontal");
-
-        
         rb.linearVelocity = new Vector2(mover * velocidad, rb.linearVelocity.y);
 
         
-        if (mover > 0)
-        {
-            sr.flipX = false; 
-        }
-        else if (mover < 0)
-        {
-            sr.flipX = true; 
-        }
+        anim.SetFloat("p_Velocidad", Mathf.Abs(mover));
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+       
+        if (mover > 0) sr.flipX = false;
+        else if (mover < 0) sr.flipX = true;
+
+        bool enAire = Mathf.Abs(rb.linearVelocity.y) > 0.1f;
+        anim.SetBool("p_estaSaltando", enAire);
+
+        if (Input.GetButtonDown("Jump") && !enAire)
         {
             rb.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
         }
